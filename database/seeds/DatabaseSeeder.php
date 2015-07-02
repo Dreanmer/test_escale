95f730b7e9eb1b31ccdc4f849abd960eb53b4c72
Alexandre Reis <dreanmer@gmail.com>
2015-07-02 14:27:37 -0300

    user seeder;

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,20 +2,37 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User as User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+		$this->call(UserTableSeeder::class);
 
-        Model::reguard();
-    }
+		Model::reguard();
+	}
+
+}
+
+class UserTableSeeder extends Seeder
+{
+
+	public function run()
+	{
+		User::truncate();
+		User::create([
+			'email' => 'admin@test.com',
+			'password' => Hash::make('password'),
+			'name' => 'Admin',
+		]);
+	}
 }
