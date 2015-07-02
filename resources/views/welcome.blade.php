a216fbabd2d60accef39b407e58afd9db029842e
Alexandre Reis <dreanmer@gmail.com>
2015-07-02 16:12:12 -0300

    auto complete por ajax; flash success message; add code; add helper links;

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,16 +16,21 @@
     <body>
         <div class="container">
 
-            <form class="form-where-u">
+            <h3>Access code: {{ $code }} </h3>
+
+            <p>{{ $message }}</p>
+
+            <form class="form-where-u" method="post">
+                {!! csrf_field() !!}
                 <h2 class="form-where-u-heading">Onde você esta?</h2>
                 <label for="inputCep" class="sr-only">Cep</label>
-                <input type="text" id="inputCep" class="form-control" placeholder="CEP" required autofocus>
+                <input type="text" name="cep" id="inputCep" class="form-control" placeholder="CEP" required autofocus>
                 <label for="inputCity" class="sr-only">Cidade</label>
-                <input type="text" id="inputCity" class="form-control" placeholder="Cidade" required disabled>
+                <input type="text" name="city" id="inputCity" class="form-control" placeholder="Cidade" required disabled>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Enviar</button>
             </form>
 
-            <a href="{{ url('logs') }}">Ver Logs.</a>
+            <a href="{{ url('logs') }}">Ver Logs.</a> <a href="{{ url('clearSession') }}">Clear Session.</a>
 
         </div> <!-- /container -->
     </body>
